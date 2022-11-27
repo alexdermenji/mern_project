1bcb7584012f85c5fd5d6bcbcca3a5ca7ad2a1e0
Alexandr Dermenji <allexdermenji@gmail.com>
2022-11-27 18:35:10 +0000

    setting graphql

---
+++ server/sampleData.js
@@ -0,0 +1,49 @@
+const projects = [
+  {
+    id: '1',
+    clientId: '1',
+    name: 'Project 1',
+    description: 'Project 1 description',
+    status: 'active',
+  },
+  {
+    id: '2',
+    clientId: '2',
+    name: 'Project 2',
+    description: 'Project 2 description',
+    status: 'active',
+  },
+  {
+    id: '3',
+    clientId: '3',
+    name: 'Project 3',
+    description: 'Project 3 description',
+    status: 'active',
+  },
+];
+
+const clients = [
+  {
+    id: '1',
+    name: 'Client 1',
+    email: 'Tony Stark',
+    phone: '123-456-7890',
+  },
+  {
+    id: '2',
+    name: 'Client 2',
+    email: 'Steve Rogers',
+    phone: '123-456-7890',
+  },
+  {
+    id: '3',
+    name: 'Client 3',
+    email: 'Bruce Banner',
+    phone: '123-456-7890',
+  },
+];
+
+module.exports = {
+  projects,
+  clients,
+};
